6b4d96c86172119ff2cca2859429a27060d1b0a0
Rob Houck <rbhouck32@gmail.com>
2020-03-22 18:24:56 -0500

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -5,3 +5,9 @@ import App from "./App";
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+// test("Max Length is equal to 3 characters", () => {
+//   let expected = 3;
+//   let actual = register({ maxLength });
+//   expected(actual).toBe(expected);
+// });
