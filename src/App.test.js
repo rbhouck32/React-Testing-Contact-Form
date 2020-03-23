ef10e48ffae62f8ce0b4d19abb45c1a2cdd3cf09
Rob Houck <rbhouck32@gmail.com>
2020-03-22 20:02:39 -0500

    this is the initialization of two tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
+import ContactForm from "./components/ContactForm";
+import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
 
 test("renders App without crashing", () => {
   render(<App />);
+});
+
+test("First Name input Field", () => {
+  const { getByText } = render(<ContactForm />);
+
+  const firstName = getByText(/firstName\*/i);
+
+  expect(firstName).toBeInTheDocument();
 });
 
 // test("Max Length is equal to 3 characters", () => {
